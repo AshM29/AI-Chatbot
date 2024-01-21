fcafcead18d544409cf017f3204b79fcb9ba3e0b
Asmita Mukherjee <85896644+AshM29@users.noreply.github.com>
2024-01-21 19:37:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ model=genai.configure(api_key="GOOGLE_API_KEY")
 chat=genai.start_chat(history=[])
 
 def get_gemini_response(question):
-  response=chat.send_message(stream=True)
+  response=chat.send_message(question,stream=True)
   return response
   
 st._config("Q&A Chatbot")
 st.header("Conversational ChatBot")
 
-if 'chat_history' not in st.session_state['chat_history']:
+if 'chat_history' not in st.session_state:
     st.session_state['chat_history']= []
 
 input=st.text_input("Input: ",key="input")
@@ -23,4 +23,12 @@ submit=st.button("Send")
 if input and submit:
   response=get_gemini_response(input)
   st.session_state['chat_history'].append(("You",input))
+  st.subheader("Response: ")
+  for chunk in response:
+    st.write(chunk.text)
+    st.session_state['chat_history'].append(("Response",chunk.text))
+  
+st.subheader("Chat history: ")
+for role,response in  st.session_state['chat_history']:
+  st.write(f"{role}: {response}")
   
